c3aa8df6df5c46902003fd0e0d8cab9bb7b9cd1f
Michel SLAGMULDER <michel.slagmulder.external@atos.net>
2023-06-03 10:16:20 +0200

    end of line fix

--- extension.js
+++ extension.js
@@ -2,11 +2,17 @@
 const vscode = require('vscode');
 
 
-function transform(s) {
+function transform(document,selection) {
 	let config = vscode.workspace.getConfiguration('csv2table')
 	let delimiter = config.get("delimiter",";")
 	let header = config.get("header", false)
-	let lines = s.split("\n")
+
+	let st = selection.start.line
+	let e = selection.end.line
+	let lines = []
+	for (let i = st;i<=e;i++) {
+		lines.push(document.lineAt(i).text)
+	}
 	let result = ""
 	if (header) {
 		let firstline="";
@@ -24,17 +30,13 @@ function transform(s) {
 
 function activate(context) {
 
-	console.log('Congratulations, your extension "csv2table" is now active!');
 
 	let disposable = vscode.commands.registerCommand('csv2table.toHTML', function () {
-		vscode.window.showInformationMessage('Hello World from CSV2Table!');
+		//vscode.window.showInformationMessage('Hello World from CSV2Table!');
 		let textEditor = vscode.window.activeTextEditor
 		let selection = textEditor.selection
 		let document = textEditor.document
-		let start = selection.start
-		let end = selection.end
-		let text = document.getText(new vscode.Range(start,end))
-		let result = transform(text)
+		let result = transform(document,selection)
 		textEditor.edit(async (ed)=> {
 			await ed.replace(selection,result)
 			vscode.commands.executeCommand("editor.action.formatDocument")
