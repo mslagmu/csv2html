c573980397470694822b5b6317418e539fe62156
Michel SLAGMULDER <michel.slagmulder.external@atos.net>
2023-06-07 10:17:32 +0200

    add footer

--- extension.js
+++ extension.js
@@ -6,6 +6,7 @@ function transform(document,selection) {
 	let config = vscode.workspace.getConfiguration('csv2table')
 	let delimiter = config.get("delimiter",";")
 	let header = config.get("header", false)
+	let footer = config.get("footer", false)
 
 	let st = selection.start.line
 	let e = selection.end.line
@@ -17,14 +18,20 @@ function transform(document,selection) {
 	if (header) {
 		let firstline="";
 		[firstline,...lines] = lines
-		result = "<tr>\n<th>" + firstline.replaceAll(delimiter,"</th>\n<th>") + "</th>\n</tr>\n" 
+		result = "<thead><tr>\n<th>" + firstline.replaceAll(delimiter,"</th>\n<th>") + "</th>\n</tr>\n</thead>" 
+	}
+	footerresult = ""
+	if (footer) {
+		let lastline="";
+		lastline=lines.pop()
+		footerresult = "<tfoot><tr>\n<td>" + lastline.replaceAll(delimiter,"</td>\n<td>") + "</td>\n</tr>\n</tfoot>" 
 	}
 	result = lines.reduce((accu,current)=> {
 		accu += "<tr>\n<td>" + current.replaceAll(delimiter,"</td>\n<td>") + "</td>\n</tr>\n"
 		return accu
 	},result)
 
-	return "<table>\n"+result+"</table>"
+	return "<table>\n"+result+footerresult+"</table>"
 }
 
 
